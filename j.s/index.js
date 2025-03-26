fa0920c5300b7a014674299e688887172e65c784
Elvis <elvis.wachira@student.moringaschool.com>
2025-03-26 20:52:46 +0300

    nav-color

--- j.s/index.js
+++ j.s/index.js
@@ -1,4 +1,15 @@
-document.getElementById("myButton")
+document.getElementById("home")
 .addEventListener("click", function(){
-    
+    alert("Welcome to our services!")
+})
+
+document.getElementById("menu")
+.addEventListener("click", function(){
+    document.getElementById("appetizer").scrollIntoView({behavior: "smooth"});
+});
+
+document.querySelectorAll(".add-to-cart").forEach(button=>{
+    button.addEventListener("click", function (){
+        alert("Item added to cart!")
+    })
 })
