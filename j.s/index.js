eee7bffdb7aac70e8026be64bf5c864c6554a0c4
Elvis <elvis.wachira@student.moringaschool.com>
2025-03-27 00:59:39 +0300

    js-fetch-json-server

--- j.s/index.js
+++ j.s/index.js
@@ -1,7 +1,22 @@
-const newRecipe ={
-    name:"Lobster Ravioli",
-    description: "Pasta stuffed with lobster in saffron sauce."
-}
+fetch("http://localhost:5000/recipes/1"{
+    .then(response => response.json())
+    .then(data => displayRecipes(data))
+    .catch(error=> console.error("Error fetching recipes:",error))
+})
+
+// const newRecipe ={
+//     name:"Lobster Ravioli",
+//     description: "Pasta stuffed with lobster in saffron sauce."
+// };
+
+// fetch("http://localhost:5000/recipes",){
+//     method:"POST"
+//     headers:{
+//         "Content-Type":"application/json"
+//     },
+//     body:JSON.stringify(newRecipe)
+// })
+
 
 document.getElementById("home")
 .addEventListener("click", function(){
